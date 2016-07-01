3b99b4daea9ddb792498f189d0824e3b2665e492
codegenius <avenord.dev@gmail.com>
2016-07-01 18:22:20 +0400

    finished with settings menu

---
+++ protocol.swift
@@ -0,0 +1,13 @@
+//
+//  protocol.swift
+//  YouTube
+//
+//  Created by Adam Venord on 7/1/16.
+//  Copyright © 2016 Adam Venord. All rights reserved.
+//
+
+import Foundation
+
+public protocol SelectedCell {
+    func cellInfoAtIndex(index : Int)
+}
